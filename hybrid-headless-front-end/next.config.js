f102685ab1e895d5a2a676454229577589c30a6f
Tim Dobson <hello@tdobson.net>
2024-12-18 14:40:54 +0000

    style: Apply consistent indentation with tabs

--- hybrid-headless-front-end/next.config.js
+++ hybrid-headless-front-end/next.config.js
@@ -1,23 +1,23 @@
 const nextConfig = {
-    output: "standalone",
-    images: {
-        domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
-        unoptimized: true,
-    },
-    webpack: (config, { dev, isServer }) => {
-        if (!dev) {
-            config.optimization = {
-                ...config.optimization,
-                minimize: true,
-                splitChunks: {
-                    chunks: "all",
-                    minSize: 20000,
-                    maxSize: 244000,
-                },
-            };
-        }
-        return config;
-    },
+	output: "standalone",
+	images: {
+		domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
+		unoptimized: true,
+	},
+	webpack: (config, { dev, isServer }) => {
+		if (!dev) {
+			config.optimization = {
+				...config.optimization,
+				minimize: true,
+				splitChunks: {
+					chunks: "all",
+					minSize: 20000,
+					maxSize: 244000,
+				},
+			};
+		}
+		return config;
+	},
 };
 
 module.exports = nextConfig;
