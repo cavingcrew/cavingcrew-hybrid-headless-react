b8280f91a4cb7ba9cd504633b740c73eac3450a7
Tim Dobson <hello@tdobson.net>
2024-12-18 14:40:44 +0000

    refactor: Remove Mantine ESM aliases and update package versions

--- hybrid-headless-front-end/next.config.js
+++ hybrid-headless-front-end/next.config.js
@@ -1,30 +1,23 @@
 const nextConfig = {
-	output: "standalone",
-	images: {
-		domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
-		unoptimized: true,
-	},
-	webpack: (config, { dev, isServer }) => {
-		if (!dev) {
-			config.optimization = {
-				...config.optimization,
-				minimize: true,
-				splitChunks: {
-					chunks: "all",
-					minSize: 20000,
-					maxSize: 244000,
-				},
-			};
-		}
-		if (!isServer) {
-			config.resolve.alias = {
-				...config.resolve.alias,
-				"@mantine/core": "@mantine/core/esm",
-				"@mantine/hooks": "@mantine/hooks/esm",
-			};
-		}
-		return config;
-	},
+    output: "standalone",
+    images: {
+        domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
+        unoptimized: true,
+    },
+    webpack: (config, { dev, isServer }) => {
+        if (!dev) {
+            config.optimization = {
+                ...config.optimization,
+                minimize: true,
+                splitChunks: {
+                    chunks: "all",
+                    minSize: 20000,
+                    maxSize: 244000,
+                },
+            };
+        }
+        return config;
+    },
 };
 
 module.exports = nextConfig;
