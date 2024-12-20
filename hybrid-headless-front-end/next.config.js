dd86305b19fea4cf5d5ae5100f8c6e45b3d90a70
Tim Dobson <hello@tdobson.net>
2024-12-20 09:15:28 +0000

    style: Apply linter formatting to next.config.js

--- hybrid-headless-front-end/next.config.js
+++ hybrid-headless-front-end/next.config.js
@@ -1,13 +1,13 @@
 const nextConfig = {
-  output: "export",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
-    unoptimized: true,
-  },
-  trailingSlash: true,
+	output: "export",
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
+		unoptimized: true,
+	},
+	trailingSlash: true,
 };
 
 module.exports = nextConfig;
