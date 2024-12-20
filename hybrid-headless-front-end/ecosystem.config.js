435576385b59474d71f35006507a62411d882eb4
Tim Dobson <hello@tdobson.net>
2024-12-20 10:19:22 +0000

    style: Apply linter formatting to ecosystem.config.js

--- hybrid-headless-front-end/ecosystem.config.js
+++ hybrid-headless-front-end/ecosystem.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  apps: [{
-    name: 'hybrid-headless-frontend',
-    script: 'node_modules/next/dist/bin/next',
-    args: 'start',
-    env: {
-      PORT: 3000,
-      NODE_ENV: 'production',
-    },
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-  }]
+	apps: [
+		{
+			name: "hybrid-headless-frontend",
+			script: "node_modules/next/dist/bin/next",
+			args: "start",
+			env: {
+				PORT: 3000,
+				NODE_ENV: "production",
+			},
+			instances: 1,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: "1G",
+		},
+	],
 };
