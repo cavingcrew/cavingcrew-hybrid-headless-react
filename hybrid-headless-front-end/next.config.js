89773ba90ea799f4c16c27564793a237a4f7ca58
Tim Dobson <hello@tdobson.net>
2024-12-20 09:30:11 +0000

    refactor: Remove rewrites config for static export compatibility

--- hybrid-headless-front-end/next.config.js
+++ hybrid-headless-front-end/next.config.js
@@ -8,15 +8,6 @@ const nextConfig = {
 		unoptimized: true,
 	},
 	trailingSlash: true,
-	async rewrites() {
-		return [
-			{ source: "/trips", destination: "/trips" },
-			{ source: "/trips/:slug", destination: "/trips/:slug" },
-			{ source: "/categories", destination: "/categories" },
-			{ source: "/categories/:slug", destination: "/categories/:slug" },
-			{ source: "/:path*", destination: "/:path*" },
-		];
-	},
 };
 
 module.exports = nextConfig;
