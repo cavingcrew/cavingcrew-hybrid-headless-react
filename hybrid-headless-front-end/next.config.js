064494eb5886e0fd34833cc42c8069b3e49da2ed
Tim Dobson <hello@tdobson.net>
2025-01-05 17:10:36 +0000

    style: Format code with linter

--- hybrid-headless-front-end/next.config.js
+++ hybrid-headless-front-end/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
 	images: {
 		domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
 	},
-	transpilePackages: ['@tanstack/react-query'],
+	transpilePackages: ["@tanstack/react-query"],
 	webpack: (config) => {
 		config.resolve.fallback = { fs: false };
 		return config;
