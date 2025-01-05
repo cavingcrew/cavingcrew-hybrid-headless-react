03442180cdc59698a5c32611cbbb489c6a054347
Tim Dobson <hello@tdobson.net>
2025-01-05 15:42:46 +0000

    refactor: Reorganize imports and adjust indentation in CategoryTripsGrid component

--- hybrid-headless-front-end/components/categories/CategoryTripsGrid.tsx
+++ hybrid-headless-front-end/components/categories/CategoryTripsGrid.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { SimpleGrid } from '@mantine/core';
-import { TripCard } from '../trips/TripCard';
-import type { Trip } from '../../types/api';
+import { SimpleGrid } from "@mantine/core";
+import React from "react";
+import type { Trip } from "../../types/api";
+import { TripCard } from "../trips/TripCard";
 
 interface CategoryTripsGridProps {
-  trips: Trip[];
+	trips: Trip[];
 }
 
 export function CategoryTripsGrid({ trips }: CategoryTripsGridProps) {
-  return (
-    <SimpleGrid cols={{ base: 1, sm: 2, md: 3 }} spacing="lg">
-      {trips.map((trip) => (
-        <TripCard key={trip.id} trip={trip} />
-      ))}
-    </SimpleGrid>
-  );
+	return (
+		<SimpleGrid cols={{ base: 1, sm: 2, md: 3 }} spacing="lg">
+			{trips.map((trip) => (
+				<TripCard key={trip.id} trip={trip} />
+			))}
+		</SimpleGrid>
+	);
 }
