87718396120c28b294c383a32d046798d0319a9b
Tim Dobson <hello@tdobson.net>
2025-01-27 22:56:10 +0000

    refactor: Remove duplicate code and clean up ClientNavigationProvider

--- hybrid-headless-front-end/next.config.js
+++ hybrid-headless-front-end/next.config.js
@@ -1,30 +1,30 @@
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
-    unoptimized: true,
-  },
-  experimental: {
-    optimizeCss: {
-      critters: {
-        preload: "media",
-        preloadFonts: true,
-      },
-    },
-    scrollRestoration: true,
-  },
-  transpilePackages: ["@tanstack/react-query"],
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-  output: "export",
-  trailingSlash: true,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
+		unoptimized: true,
+	},
+	experimental: {
+		optimizeCss: {
+			critters: {
+				preload: "media",
+				preloadFonts: true,
+			},
+		},
+		scrollRestoration: true,
+	},
+	transpilePackages: ["@tanstack/react-query"],
+	webpack: (config) => {
+		config.resolve.fallback = { fs: false };
+		return config;
+	},
+	compiler: {
+		removeConsole: process.env.NODE_ENV === "production",
+	},
+	output: "export",
+	trailingSlash: true,
 };
 
 module.exports = nextConfig;
