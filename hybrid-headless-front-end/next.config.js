49cece41fab81a3bb39d598efa7a262da7154ad1
Tim Dobson <hello@tdobson.net>
2025-01-27 22:15:20 +0000

    style: Apply linter formatting to next.config.js

--- hybrid-headless-front-end/next.config.js
+++ hybrid-headless-front-end/next.config.js
@@ -1,43 +1,43 @@
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
-  },
-  experimental: {
-    optimizeCss: {
-      critters: {
-        preload: 'media',
-        preloadFonts: true,
-      }
-    },
-    scrollRestoration: true,
-  },
-  transpilePackages: ["@tanstack/react-query"],
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
+	},
+	experimental: {
+		optimizeCss: {
+			critters: {
+				preload: "media",
+				preloadFonts: true,
+			},
+		},
+		scrollRestoration: true,
+	},
+	transpilePackages: ["@tanstack/react-query"],
+	webpack: (config) => {
+		config.resolve.fallback = { fs: false };
 
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["next/babel"],
-          plugins: [["@babel/plugin-transform-runtime", { regenerator: true }]],
-        },
-      },
-    });
+		config.module.rules.push({
+			test: /\.(js|jsx|ts|tsx)$/,
+			exclude: /node_modules/,
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: ["next/babel"],
+					plugins: [["@babel/plugin-transform-runtime", { regenerator: true }]],
+				},
+			},
+		});
 
-    return config;
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-  pageExtensions: ["tsx", "ts"],
-  reactStrictMode: true,
-  poweredByHeader: false,
+		return config;
+	},
+	compiler: {
+		removeConsole: process.env.NODE_ENV === "production",
+	},
+	pageExtensions: ["tsx", "ts"],
+	reactStrictMode: true,
+	poweredByHeader: false,
 };
 
 module.exports = nextConfig;
