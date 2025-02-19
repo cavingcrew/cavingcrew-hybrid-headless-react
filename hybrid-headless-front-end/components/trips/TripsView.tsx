b14368b6265bb1d356e78843ff04f8d0676ffa9b
Tim Dobson <hello@tdobson.net>
2025-02-19 02:09:24 +0000

    fix: Correct Group component wrap prop type to string value

--- hybrid-headless-front-end/components/trips/TripsView.tsx
+++ hybrid-headless-front-end/components/trips/TripsView.tsx
@@ -157,7 +157,7 @@ export function TripsView({ trips }: TripsViewProps) {
         {/* Filter Controls */}
         <Group 
           gap="xs" 
-          wrap
+          wrap="wrap"
           justify={{ sm: 'center' }}
         >
           {[
