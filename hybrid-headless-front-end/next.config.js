7c5b94749e9494f338f9146687d90180c58170b4
Tim Dobson <hello@tdobson.net>
2025-03-08 19:55:48 +0000

    refactor: Simplify Next.js configuration by removing experimental and output settings

--- hybrid-headless-front-end/next.config.js
+++ hybrid-headless-front-end/next.config.js
@@ -4,27 +4,12 @@ const nextConfig = {
 	},
 	images: {
 		domains: ["localhost", "www.cavingcrew.com", "cavingcrew.com"],
-		unoptimized: true,
-	},
-	experimental: {
-		optimizeCss: {
-			critters: {
-				preload: "media",
-				preloadFonts: true,
-			},
-		},
-		scrollRestoration: true,
 	},
 	transpilePackages: ["@tanstack/react-query"],
 	webpack: (config) => {
 		config.resolve.fallback = { fs: false };
 		return config;
 	},
-	compiler: {
-		removeConsole: process.env.NODE_ENV === "production",
-	},
-	output: "export",
-	trailingSlash: true,
 };
 
 module.exports = nextConfig;
