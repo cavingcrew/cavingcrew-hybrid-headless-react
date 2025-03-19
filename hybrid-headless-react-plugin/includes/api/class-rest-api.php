de97f5149deead97741017ba4a6bf27526f1e303
Tim Dobson <hello@tdobson.net>
2025-03-19 22:19:25 +0000

    fix: Remove non-existent Caving Crew Controller reference

--- hybrid-headless-react-plugin/includes/api/class-rest-api.php
+++ hybrid-headless-react-plugin/includes/api/class-rest-api.php
@@ -52,7 +52,6 @@ class Hybrid_Headless_Rest_API {
         new Hybrid_Headless_User_Controller();
         new Hybrid_Headless_Trip_Participants_Controller();
         new Hybrid_Headless_Trip_Reports_Controller();
-        new Hybrid_Headless_Caving_Crew_Controller();
     }
 
     /**
