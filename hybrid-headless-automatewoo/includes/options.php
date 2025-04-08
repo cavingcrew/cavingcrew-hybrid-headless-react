3dac34ed705ddfac8e963c66485c7fda51ac8d8f
Tim Dobson <hello@tdobson.net>
2025-04-08 13:50:13 +0100

    fix: remove duplicate class definition in options.php

--- hybrid-headless-automatewoo/includes/options.php
+++ hybrid-headless-automatewoo/includes/options.php
@@ -3,22 +3,6 @@ namespace HybridHeadlessAutomateWoo;
 
 use AutomateWoo\Options_API;
 
-defined('ABSPATH') || exit;
-
-class Options extends Options_API {
-    public $prefix = 'hybrid_headless_automatewoo_';
-
-    public function __construct() {
-        $this->defaults = [
-            // Add any default settings here
-        ];
-    }
-}
-<?php
-namespace HybridHeadlessAutomateWoo;
-
-use AutomateWoo\Options_API;
-
 defined( 'ABSPATH' ) || exit;
 
 class Options extends Options_API {
